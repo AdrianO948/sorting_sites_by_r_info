a514969ebb46158bf9e8b7952eab3df9cbb98163
Adrian Ochnik <adrian.ochnik@onet.com.pl>
2023-08-10 13:36:49 +0200

    Add try except statment, file writers and import requests

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import requests
+
+
+with open('sites_to_sort.txt', 'r')as f:
+    content = f.read()
+    sitesListed = content.split(',')
+
+dictOfResponses = {}
+for site in sitesListed:
+    dictOfResponses[site] = requests.get(site).status_code
+
+for key, value in dictOfResponses.items():
+    try:
+        with open(f'{value}.txt', 'a') as f:
+            f.write(key + '\n')
+    except FileNotFoundError:
+        with open(f'{value}.txt', 'w')as f:
+            f.write(key + '\n')
+
 
