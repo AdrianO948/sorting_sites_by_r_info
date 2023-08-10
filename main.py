8a9a58c360afc68d2ed5d35dfef1e5e7709add9e
Adrian Ochnik <adrian.ochnik@onet.com.pl>
2023-08-10 14:31:08 +0200

    Add exception handling in 'open_file_and_write_content_into_list' function to make sure that everything works properly

--- main.py
+++ main.py
@@ -2,20 +2,23 @@ import requests
 
 
 def open_file_and_write_content_into_list(file_name):
-    try:
-        file_to_read = open(f'{file_name}', 'r')
-    except FileNotFoundError:
-        return 'File not found!'
-    else:
-        split_lines_list = file_to_read.read().split(',')
-        file_to_read.close()
-        return split_lines_list
+    while True:
+        try:
+            file_to_read = open(f'{file_name}', 'r')
+        except FileNotFoundError:
+            file_name = input('File not found! Please enter valid file name: ')
+            continue
+        else:
+            split_lines_list = file_to_read.read().split(',')
+            file_to_read.close()
+            return split_lines_list
 
 
 def getting_requests_codes_into_dict(list_of_sites):
     dict_of_responses = {}
     for site in list_of_sites:
         dict_of_responses[site] = requests.get(site).status_code
+
     return dict_of_responses
 
 
@@ -29,7 +32,7 @@ def writing_request_codes_to_files(dictionary_of_responses):
                 f.write(key + '\n')
 
 
-fileName = 'sites_to_sort.txt'
+fileName = 'sites_.txt'
 listedSites = open_file_and_write_content_into_list(fileName)
 dictOfResponses = getting_requests_codes_into_dict(listedSites)
 writing_request_codes_to_files(dictOfResponses)
